4c55fcf6c87619ba010b8c32d21a1c4911052d2d
Geovane Soares <57137423+geovanedcs@users.noreply.github.com>
2023-12-11 10:52:52 -0300

    Update accepts_nested_attributes_for in Cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :notepad
   has_many :products, dependent: :destroy
-  accepts_nested_attributes_for :products, reject_if: lambda { |a| a[:name, :amount].blank?}, allow_destroy: true
+  accepts_nested_attributes_for :products, reject_if: lambda { |a| a[:name].blank? || a[:amount].blank?}, allow_destroy: true
 end
